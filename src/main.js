0adb4d2dbecaec60f3610559d2238c99b4c29896
iti-rina <itigecheva.irina@gmail.com>
2025-02-17 21:06:44 +0700

    fix: move all renders to presenter; add missed tag

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
-import { render } from './render';
-import FilterView from './view/filter-view';
 import EventListPresenter from './presenter/event-list-presenter';
-
-const filterContainer = document.querySelector('.trip-controls__filters');
-render(new FilterView(), filterContainer);
 
 const eventListPresenter = new EventListPresenter({
   eventListContainer: document.querySelector('.trip-events'),
+  filterContainer: document.querySelector('.trip-controls__filters'),
 });
 
 eventListPresenter.init();
--- src/presenter/event-list-presenter.js
+++ src/presenter/event-list-presenter.js
@@ -3,15 +3,19 @@ import CreatePointView from '../view/create-point-view.js';
 import EventListView from '../view/event-list-view.js';
 import EventPointView from '../view/event-point-view.js';
 import SortView from '../view/sort-view.js';
+import FilterView from '../view/filter-view';
 
-export default class EventListPresenter {
+export default class EventAppPresenter {
+  filterComponent = new FilterView();
   eventListComponent = new EventListView();
 
-  constructor({ eventListContainer }) {
+  constructor({ eventListContainer, filterContainer }) {
     this.eventListContainer = eventListContainer;
+    this.filterContainer = filterContainer;
   }
 
   init() {
+    render(new FilterView(), this.filterContainer);
     render(new SortView(), this.eventListContainer);
     render(this.eventListComponent, this.eventListContainer);
     render(new CreatePointView(), this.eventListComponent.getElement());
